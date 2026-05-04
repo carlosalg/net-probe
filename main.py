36c18900bc1241ebe12243d2ef9952187831b14c
carlosalg <cabetolg98@gmail.com>
2026-05-04 11:26:28 -0500

    trying sending command and configs to cisco IOS XR, with netmiko plugin

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
 from nornir import InitNornir
+from nornir_netmiko.tasks import netmiko_send_config
 from nornir_netmiko.tasks import netmiko_send_command
 from nornir_utils.plugins.functions import print_result
+from nornir.core.task import Task, Result
 
+def gather_facts(task: Task) -> Result:
+    version = task.run(
+        task=netmiko_send_command,
+        command_string="show version"
+    )
+    interfaces = task.run(
+        task=netmiko_send_command,
+        command_string="show ip interface brief"
+    )
+    return Result(host=task.host)
+def push_config(task: Task):
+    task.run(
+        task=netmiko_send_config,
+        config_commands=[
+            "interface Loopback100",
+            "description managed by nornir",
+        ]
+    )
 nr = InitNornir(config_file="config.yaml")
+result_facts = nr.run(task=gather_facts)
+result_config = nr.run(task=push_config)
 
-result = nr.run(
-    task=netmiko_send_command, command_string="show ip interface brief | excl down"
-               )
-print_result(result)
+print_result(result_facts)
+print_result(result_config)
 
 
